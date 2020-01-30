0012ffac0f7e01368151b5464e31c95992531ef4
OttoCodeBerlin <ottoschmidberlin@gmail.com>
2020-01-30 13:26:25 +0100

    Added component and page structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+// Pages
+import home from './pages/home'
+import login from './pages/login'
+import signup from './pages/signup'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Our App</h1>
+        <Router>
+          <Switch>
+            <Route path="/" component={home} />
+            <Route path="/login" component={login} />
+            <Route path="/signup" component={signup} />
+          </Switch>
+        </Router>
       </div>
     )
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+// remove export here
+export class Navbar extends Component {
+  render() {
+    return <div></div>
+  }
+}
+
+export default Navbar
